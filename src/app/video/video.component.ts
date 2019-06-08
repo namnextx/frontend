a46e51ad3e1f1dd8056274bbe5c438c2936a055d
nguyenanh <nguyenanhtran2406@gmail.com>
2019-06-09 00:37:04 +0700

    nguyenanhcm

--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-video',
@@ -7,10 +7,40 @@ import {Component, OnInit} from '@angular/core';
 })
 export class VideoComponent implements OnInit {
 
+  @Input()
+  dataQuestion: boolean;
+  @ViewChild('videoPlayer') videoPlayer: ElementRef;
+  check = true;
+
   constructor() {
   }
 
   ngOnInit() {
+    this.reload();
+  }
+
+  play() {
+    if (this.dataQuestion === true) {
+      this.playPause();
+    }
+  }
+
+  reload() {
+    this.dataQuestion = true;
+  }
+
+  playPause() {
+    if (this.check) {
+      this.videoPlayer.nativeElement.play();
+      setTimeout(() => {
+        this.pause();
+      }, 3000);
+    }
+    this.dataQuestion = false;
+  }
+
+  pause() {
+    this.videoPlayer.nativeElement.pause();
   }
 
 }
