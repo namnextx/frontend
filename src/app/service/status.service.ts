f9a38355f883acfae949b69956ab70e66e13dbf6
Namnextx <phuongnam.nextx@gmail.com>
2019-06-09 07:48:07 +0700

    done

---
+++ src/app/service/status.service.ts
@@ -0,0 +1,18 @@
+import {Injectable} from '@angular/core';
+import {BehaviorSubject} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StatusService {
+  private status = new BehaviorSubject(false);
+  currentStatus = this.status.asObservable();
+
+  constructor() {
+  }
+
+  changeStatus(statusPram: boolean) {
+    this.status.next(statusPram);
+  }
+
+}
